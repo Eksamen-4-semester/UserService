703fda68d1198a56c6a2a08b26c44fbb8cb8a801
MortenStaunstrup <mort490fgromling@gmail.com>
2026-05-11 11:08:02 +0200

    Added version controller and endpoint

--- UserAPI/Controllers/AdminController.cs
+++ UserAPI/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using UserAPI.Models;
 using UserAPI.Repositories.Interfaces;
 
@@ -66,5 +67,25 @@ public class AdminController : ControllerBase
         return BadRequest("Admin already exists");
     }
     
+    [HttpGet("version")]
+    public async Task<Dictionary<string,string>> GetVersion()
+    {
+        var properties = new Dictionary<string, string>();
+        var assembly = typeof(Program).Assembly;
+        properties.Add("service", "FitLife Auth service");
+        var ver = FileVersionInfo.GetVersionInfo(typeof(Program)
+            .Assembly.Location).ProductVersion;
+        properties.Add("version", ver!);
+        try {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString();
+            properties.Add("hosted-at-address", ipa);
+        } catch (Exception ex) {
+            _logger.LogError(ex.Message);
+            properties.Add("hosted-at-address", "Could not resolve IP-address");
+        }
+        return properties;
+    }
     
 }
---
+++ UserAPI/Controllers/VersionController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class VersionController : ControllerBase
+{
+    private readonly ILogger<VersionController> _logger;
+
+    public VersionController(
+        ILogger<VersionController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<Dictionary<string,string>> GetVersion()
+    {
+        _logger.LogInformation("Version controll endpoint called for AuthService");
+        var properties = new Dictionary<string, string>();
+        var assembly = typeof(Program).Assembly;
+        properties.Add("service", "FitLife Auth service");
+        var ver = FileVersionInfo.GetVersionInfo(typeof(Program)
+            .Assembly.Location).ProductVersion;
+        properties.Add("version", ver!);
+        try {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString();
+            properties.Add("hosted-at-address", ipa);
+        } catch (Exception ex) {
+            _logger.LogError(ex.Message);
+            properties.Add("hosted-at-address", "Could not resolve IP-address");
+        }
+        return properties;
+    }
+}
